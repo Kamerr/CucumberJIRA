c14203ffa6fbe1adc1e1e7ed514f8abf84b200a7
recep <recepacar1979@gmail.com>
2023-06-21 11:45:19 +0300

    Add to Cart_2 US_18

--- src/test/java/com/ourvirtualmarket/pages/ProductPage.java
+++ src/test/java/com/ourvirtualmarket/pages/ProductPage.java
@@ -5,9 +5,8 @@ import com.ourvirtualmarket.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
 
-public class ProductsPage extends BasePage {
+public class ProductPage extends BasePage {
 
     @FindBy(xpath = "//div[@class='container']/div/span")
     public WebElement allCategoryButton;
--- src/test/java/com/ourvirtualmarket/step_defs/Products_StepDefs.java
+++ src/test/java/com/ourvirtualmarket/step_defs/Products_StepDefs.java
@@ -1,44 +1,42 @@
 package com.ourvirtualmarket.step_defs;
 
-import com.ourvirtualmarket.pages.ProductsPage;
+import com.ourvirtualmarket.pages.ProductPage;
 import com.ourvirtualmarket.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.remote.DriverCommand;
 
-import java.util.Map;
 public class Products_StepDefs {
 
-    ProductsPage productsPage = new ProductsPage();
+    ProductPage productPage = new ProductPage();
 
     @When("The user should be able to select {string} category")
     public void theUserShouldBeAbleToSelectFrom(String category) {
-        productsPage.selectTheCategories(category);
+        productPage.selectTheCategories(category);
     }
 
     @When("The user should be able to select {string} LED-backlit LCD TV \" product")
     public void the_user_should_be_able_to_select_led_backlit_lcd_tv_product(String product) {
-        productsPage.selectedProduct(product);
+        productPage.selectedProduct(product);
     }
 
     @Then("The user Should be able to click ADD TO COMPARE type")
     public void theUserShouldBeAbleToClickADDTOCOMPAREType() {
-        productsPage.clickAddToCompareType();
+        productPage.clickAddToCompareType();
     }
 
     @And("The user should take success message: {string} LED-backlit LCD TV to your product comparison!\"")
     public void theUserShouldTakeSuccessMessageLEDBacklitLCDTVToYourProductComparison(String message) {
         String expectedMessage = message;
-        String actualMessage = productsPage.messageSuccess.getText();
+        String actualMessage = productPage.messageSuccess.getText();
         Assert.assertTrue(actualMessage.contains(expectedMessage));
         Driver.get().navigate().back();
     }
 
     @When("The user Should be able to click COMPARE type")
     public void theUserShouldBeAbleToClickCOMPAREType() {
-        productsPage.compareTypeInTheProductPage();
+        productPage.compareTypeInTheProductPage();
     }
 
 }
